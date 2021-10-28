c54de69f9e21ff2b2851d03555f15cbd07c4891a
SaiKrishna2681 <45065762+SaiKrishna2681@users.noreply.github.com>
2021-10-28 16:56:59 +0530

    Update Example.java

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -1,3 +1,17 @@
+import java.io.FileInputStream;
+import java.security.KeyStore;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.stereotype.*;
@@ -15,7 +29,29 @@ public class Example {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Example.class, args);
-        Conjur conjur = new Conjur();
+        
+	    
+	    final String conjurTlsCaPath = "C:\\test\\three.jks";
+		final CertificateFactory cf = CertificateFactory.getInstance("X.509");
+		final FileInputStream certIs = new FileInputStream(conjurTlsCaPath);
+		final Certificate cert = cf.generateCertificate(certIs);
+		
+		
+		
+		final KeyStore ks = KeyStore.getInstance("JKS");
+		ks.load(null);
+		ks.setCertificateEntry("conjurTlsCaPath", cert);
+
+		final TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+		tmf.init(ks);
+
+		SSLContext conjurSSLContext = SSLContext.getInstance("TLS");
+		conjurSSLContext.init(null, tmf.getTrustManagers(), null);
+
+		System.out.println("Helloworld");
+		
+		
+		Conjur conjur = new Conjur(conjurSSLContext);
 		String secret = conjur.variables().retrieveSecret("jenkins-app/web_password");
 
         
