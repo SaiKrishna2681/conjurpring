2620ec8b81975ac566ab9ab479bfa0d48c221ced
SaiKrishna2681 <45065762+SaiKrishna2681@users.noreply.github.com>
2021-11-08 18:05:28 +0530

    Update Example.java

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -29,32 +29,14 @@ public class Example {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Example.class, args);
-        
-	    
-	    	//final String conjurTlsCaPath = "/home/ec2-user/conjur-quickstart/conf/policy/conjur-myConjurAccount.der";
-		//final CertificateFactory cf = CertificateFactory.getInstance("X.509");
-		//final FileInputStream certIs = new FileInputStream(conjurTlsCaPath);
-		//final Certificate cert = cf.generateCertificate(certIs);
-		
-		
-		
-		//final KeyStore ks = KeyStore.getInstance("JKS");
-		//ks.load(null);
-		//ks.setCertificateEntry("conjurTlsCaPath", cert);
-
-		//final TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
-		//tmf.init(ks);
-
-		//SSLContext conjurSSLContext = SSLContext.getInstance("TLS");
-		//conjurSSLContext.init(null, tmf.getTrustManagers(), null);
-
+      
 		System.out.println("Helloworld");
 		
 		
 		Conjur conjur = new Conjur();
 		//String secret = conjur.variables().retrieveSecret("jenkins-app/web_password");
-	    	return	conjur.variables().retrieveSecret("jenkins-app/web_password");
-
+	    	String pwd = conjur.variables().retrieveSecret("jenkins-app/web_password");
+		System.out.println("Password : "+pwd);
         
     }
 
