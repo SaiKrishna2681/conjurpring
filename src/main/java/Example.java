d08984f6b3dfe1a3b2f9998d57af2039631e4d02
SaiKrishna2681 <45065762+SaiKrishna2681@users.noreply.github.com>
2021-11-11 13:18:10 +0530

    Update Example.java

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -41,7 +41,7 @@ public class Example {
 	    
 	     Class.forName("com.mysql.jdbc.Driver");  
       Connection con=DriverManager.getConnection(  
-      "jdbc:mysql://54.234.232.32:3306/test","root",pwd);
+      "jdbc:mysql://54.234.232.32:3306/test","demo_java",pwd);
           //test?useSSL=false
 	    PreparedStatement pstmt = con.prepareStatement("CREATE DATABASE MyDB");
      // PreparedStatement pstmt=con.prepareStatement("insert into test.emp values(?,?)"); 
