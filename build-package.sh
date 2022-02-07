4a5c618af12202876cfa28abb172ddfb1b32835e
SaiKrishna2681 <45065762+SaiKrishna2681@users.noreply.github.com>
2022-02-07 16:35:05 +0530

    Create build-package.sh

---
+++ build-package.sh
@@ -0,0 +1,10 @@
+set -euo pipefail
+
+mkdir -p maven_cache
+
+docker run \
+  --volume "${PWD}:${PWD}" \
+  --volume "maven_cache":/root/.m2 \
+  --workdir "${PWD}" \
+  tools \
+    mvn -f pom.xml package
